dbf349d881c6af9652e95dff973aec695f634abb
Lup Gabriel <gwolf2u.website@gmail.com>
2018-02-25 09:27:12 +0200

    msm8998: Added some more VoLTE props

--- system_prop.mk
+++ system_prop.mk
@@ -231,7 +231,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.data.iwlan.enable=true \
-    persist.dbg.wfc_avail_ovr=1
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
 
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
